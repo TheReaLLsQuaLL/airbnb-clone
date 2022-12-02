4d8c6b2ae99e73c725f0eb58c8e32cf591598931
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-12-02 22:08:34 +0300

    -feat- client

---
+++ client/pages/index.js
@@ -0,0 +1,25 @@
+import { sanityClient } from "../sanity";
+
+export default function Home({ properties }) {
+  console.log(properties);
+  return <>hi</>;
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[ _type == "property"]`;
+  const properties = await sanityClient.fetch(query);
+
+  if (!properties.length) {
+    return {
+      props: {
+        properties: [],
+      },
+    };
+  } else {
+    return {
+      props: {
+        properties,
+      },
+    };
+  }
+};
